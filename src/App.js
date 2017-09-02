a3e80b425708c7e8b8ab990126f7e074e3a251b4
Hrant <harvey.mirijanyan@gmail.com>
2017-09-02 14:32:36 -0700

    Add reset and sidebar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Menu from './SideBar.js';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <Menu/>
+      </div>
+    );
+  }
+}
+
+export default App;
