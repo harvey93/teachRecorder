c512f4128561bb8bf3fd0fa98bb550bdc09112c9
Hrant <harvey.mirijanyan@gmail.com>
2017-09-02 21:31:15 -0700

    Add logic for setting states for mic screen

--- src/MicScreen.js
+++ src/MicScreen.js
@@ -3,29 +3,34 @@ import React from 'react';
 class MicScreen extends React.Component{
   constructor(){
     super();
-    this.state = {"recordingState": 4};
+    this.state = {"recordingState": 1};
     this.renderButton = this.renderButton.bind(this);
+    this.changeRecordingState = this.changeRecordingState.bind(this);
+  }
+
+  changeRecordingState(state){
+    this.setState({"recordingState": state});
   }
 
   renderButton(){
     if(this.state.recordingState === 1){
       return(
-        <div className="play-button-div">
+        <div className="play-button-div" onClick={() => this.changeRecordingState(2)}>
           <h1 className='play-button'>Record</h1>
         </div>
       );
     }else if(this.state.recordingState === 2){
       return(
         <div className="play-button-div">
-          <h1 className='play-button'>Pause</h1>
-          <h1 className='play-button'>Finish</h1>
+          <h1 className='play-button' onClick={() => this.changeRecordingState(3)}>Pause</h1>
+          <h1 className='play-button' onClick={() => this.changeRecordingState(4)}>Finish</h1>
         </div>
       );
     }else if(this.state.recordingState === 3){
       return(
         <div className="play-button-div">
-          <h1 className='play-button'>Resume</h1>
-          <h1 className='play-button'>Finish</h1>
+          <h1 className='play-button' onClick={() => this.changeRecordingState(2)}>Resume</h1>
+          <h1 className='play-button' onClick={() => this.changeRecordingState(4)}>Finish</h1>
         </div>
       );
     }else if (this.state.recordingState === 4){
@@ -33,7 +38,7 @@ class MicScreen extends React.Component{
         <div>
           <h1 className='rec-message'>Your Recording Has been Sent!</h1>
           <div className="play-button-div">
-            <h1 className='play-button'>Start New Recording</h1>
+            <h1 className='play-button' onClick={() => this.changeRecordingState(2)}>Start New Recording</h1>
           </div>
         </div>
       );
