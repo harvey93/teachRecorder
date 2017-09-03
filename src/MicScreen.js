72e7d8213e5af8ed0622b4eeecebf24071d6c644
Hrant <harvey.mirijanyan@gmail.com>
2017-09-02 21:23:41 -0700

    Add different states for menu mic screen

--- src/MicScreen.js
+++ src/MicScreen.js
@@ -3,7 +3,41 @@ import React from 'react';
 class MicScreen extends React.Component{
   constructor(){
     super();
+    this.state = {"recordingState": 4};
+    this.renderButton = this.renderButton.bind(this);
+  }
 
+  renderButton(){
+    if(this.state.recordingState === 1){
+      return(
+        <div className="play-button-div">
+          <h1 className='play-button'>Record</h1>
+        </div>
+      );
+    }else if(this.state.recordingState === 2){
+      return(
+        <div className="play-button-div">
+          <h1 className='play-button'>Pause</h1>
+          <h1 className='play-button'>Finish</h1>
+        </div>
+      );
+    }else if(this.state.recordingState === 3){
+      return(
+        <div className="play-button-div">
+          <h1 className='play-button'>Resume</h1>
+          <h1 className='play-button'>Finish</h1>
+        </div>
+      );
+    }else if (this.state.recordingState === 4){
+      return(
+        <div>
+          <h1 className='rec-message'>Your Recording Has been Sent!</h1>
+          <div className="play-button-div">
+            <h1 className='play-button'>Start New Recording</h1>
+          </div>
+        </div>
+      );
+    }
   }
 
 
@@ -12,7 +46,7 @@ class MicScreen extends React.Component{
       <div className="micscreen-container">
         <div className="button-mic">
           <i className="fa fa-microphone fa-5x mic-icon" aria-hidden="true"></i>
-          <h1 className='play-button'>Tap to Record</h1>
+          {this.renderButton()}
         </div>
       </div>
     );
